311e70f6a20315973ec85849cdacfc3010e469a5
超级手艺人 <fjxmljc007@163.com>
2022-03-01 17:41:26 +0800

    添加beanstalkda

--- beanstalkd-admin/conf/bin/entrypoint.d/init_app.sh
+++ beanstalkd-admin/conf/bin/entrypoint.d/init_app.sh
@@ -11,3 +11,14 @@ chown www-data:www-data /app/storage.json
 
 BEANSTALK_SERVERS=$BEANSTALKD_PORT_11300_TCP_ADDR:${BEANSTALKD_PORT}
 
+
+sed -ir "s/'servers'.*$/'servers'=> array('Default Beanstalkd' => 'beanstalk:\/\/$BEANSTALK_SERVERS'),/g" /app/config.php
+
+if [[ $BEANSTALKD_ENABLE_AUTH == "enable" ]]; then
+	sed -ir "s/'enabled'.*$/'enabled'=> 'true',/g" /app/config.php
+fi
+sed -ir "s/'username'.*$/'username'=> '$BEANSTALKD_AUTH_USERNAME',/g" /app/config.php
+sed -ir "s/'password'.*$/'password'=> '$BEANSTALKD_AUTH_PASSWORD',/g" /app/config.php
+
+rm -rf /app/conf
+
